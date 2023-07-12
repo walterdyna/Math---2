3cfd80a88cf0fbe11a02fb79fdd11903cc832000
walterdyna <alexdyna@gmail.com>
2023-07-12 15:34:44 -0300

    Math

---
+++ math.js
@@ -0,0 +1,37 @@
+
+
+/*
+Math -> Matemática
+ - pow (2,2) / potência / 2² = 4 ---> POWER
+ - sqrt (25,2) / raiz / √ 25
+ - PI
+ - ceil / teto -> arredondar para cima ---> teto
+ - floor / chão -> arredondar para baixo ---> chao
+
+ - randow() / número aleatório entre 0 e 1
+
+*/
+
+const potencia = Math.pow(2,2)
+console.log(potencia)
+console.log("---------------------------------------")
+
+const raiz = Math.sqrt(25,2)
+console.log(raiz)
+console.log("---------------------------------------")
+
+const pi = Math.PI 
+console.log(pi)
+console.log("---------------------------------------")
+
+const teto = Math.ceil(3.14)
+console.log(teto)
+console.log("---------------------------------------")
+
+const solo = Math.floor(3.14)
+console.log(solo)
+console.log("---------------------------------------")
+
+const aleatório = Math.random()
+console.log(aleatório)
+console.log("---------------------------------------")
